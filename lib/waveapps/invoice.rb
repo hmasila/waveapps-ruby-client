5920c1137c6b8ee8e7e946b62fde4c16070de995
Hannah Masila <hannahmasila@gmail.com>
2020-05-10 00:10:56 +0300

    invoices module updated

--- lib/waveapps/invoice.rb
+++ lib/waveapps/invoice.rb
@@ -122,10 +122,12 @@ module Waveapps
 			}
 		GRAPHQL
 
-		def self.list_invoices
-  		result = Waveapps::Client.query(ListInvoicesQuery).data
-  		return nil if result.nil?
-  		result.business.invoices.edegs.map {|n| n.node}
+		def self.list_invoices(page: 1, page_size: 10, business_id:)
+  		result = Waveapps::Client.query(ListInvoicesQuery, variables: {
+        businessId: business_id, page: page, pageSize: page_size
+      }).data
+  		return nil if result.business.nil?
+  		result.business.invoices.edges.map {|n| n.node}
   	end
 
   	CreateInvoiceQuery = Waveapps::Client.parse <<-'GRAPHQL'
@@ -245,24 +247,46 @@ module Waveapps
 			}
   	GRAPHQL
 
-    # {
-		#   "input": {
-		#     "businessId": "<BUSINESS_ID>",
-		#     "customerId": "<CUSTOMER_ID>",
-		#     "items": [
-		#       {
-		#         "productId": "<PRODUCT_ID>"
-		#       }
-		#     ]
-		#   }
-		# }
-		
-  	def self.create_invoice(items: , business_id: , customer_id: )
-  		Waveapps::Client.query(CreateInvoiceQuery, variables: { 
+  	def self.create_invoice(
+      status: nil, currency: nil, title: nil, invoice_number: nil,
+      po_number: nil, invoice_date: nil, exchange_rate: nil, due_date: nil,
+      memo: nil, footer: nil, disable_amex_payments: nil, disable_credit_card_payments: nil,
+      disable_bank_payments: nil, item_title: nil, unit_title: nil, price_title: nil,
+      amount_title: nil, hide_name: nil, hide_description: nil, hide_unit: nil,
+      hide_price: nil, hide_amount: nil, items: , business_id: , customer_id: )
+
+      Waveapps::Client.query(CreateInvoiceQuery, variables: {
   			input: {
   				businessId: business_id,
   				customerId: customer_id,
-  				items: items.map { |pid| { productId: pid[:product_id] }}
+  				items: items.map { |pid| {
+            productId: pid[:product_id],
+            quantity: pid[:quantity],
+            description: pid[:description],
+            unitPrice: pid[:unit_price]
+          }},
+          status: status,
+          currency: currency,
+          title: title,
+          invoiceNumber: invoice_number,
+          poNumber: po_number,
+          invoiceDate: invoice_date,
+          exchangeRate: exchange_rate,
+          dueDate: due_date,
+          memo: memo,
+          footer: footer,
+          disableAmexPayments: disable_amex_payments,
+          disableCreditCardPayments: disable_credit_card_payments,
+          disableBankPayments: disable_bank_payments,
+          itemTitle: item_title,
+          unitTitle: unit_title,
+          priceTitle: price_title,
+          amountTitle: amount_title,
+          hideName: hide_name,
+          hideDescription: hide_description,
+          hideUnit: hide_unit,
+          hidePrice: hide_price,
+          hideAmount: hide_amount
   			}
   		})
   	end
@@ -278,7 +302,6 @@ module Waveapps
 			    }
 			  }
   		}
-
   	GRAPHQL
 
   	def self.delete_invoice(id)

363e2d2c61ddfc500cc565b437a9955b78e1940b
Hannah Masila <hannahmasila@gmail.com>
2020-05-09 03:42:32 +0300

    update lib

--- lib/waveapps.rb
+++ lib/waveapps.rb
@@ -17,14 +17,14 @@ module Waveapps
   end
 
   # Fetch latest schema on init, this will make a network request
-  # Schema = GraphQL::Client.load_schema(HTTP)
+  Schema = GraphQL::Client.load_schema(HTTP)
 
   # However, it's smart to dump this to a JSON file and load from disk
   #
   # Run it from a script or rake task
   # rake schema:dump
   #
-  Schema = GraphQL::Client.load_schema("./tmp/schema.json")
+  # Schema = GraphQL::Client.load_schema("./tmp/schema.json")
 
   Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
 

f6c4cede4006a7d8a829f7ffd0eec818e9e6cff3
Hannah Masila <hannahmasila@gmail.com>
2020-05-10 01:31:09 +0300

    add api module

---
+++ lib/waveapps/api.rb
@@ -0,0 +1,28 @@
+module Waveapps
+  class << self
+    attr_accessor :access_token
+  end
+  class Api
+    API_URL = "https://gql.waveapps.com/graphql/public"
+    HTTP = GraphQL::Client::HTTP.new(API_URL) do
+      def headers(context)
+        # Optionally set any HTTP headers
+        {
+        	"Authorization" => "Bearer #{Waveapps.access_token}"
+        }
+      end
+    end
+
+    # Fetch latest schema on init, this will make a network request
+    Schema = GraphQL::Client.load_schema(HTTP)
+
+    # However, it's smart to dump this to a JSON file and load from disk
+    #
+    # Run it from a script or rake task
+    # rake schema:dump
+    #
+    # Schema = GraphQL::Client.load_schema("./tmp/schema.json")
+
+    Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+  end
+end
